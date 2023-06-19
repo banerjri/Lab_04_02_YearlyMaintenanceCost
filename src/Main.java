e73f5c9a2a27d9e106ff39aedd835fd963c2cec7
banerjri <banerjri@mail.uc.edu>
2023-06-18 22:13:21 -0500

    done project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double summerCost = 200.50;
+        double winterCost = 300.3;
+        double springCost = 100.75;
+        double fallCost = 50.67;
+        double TotalYearlyCost;
+        TotalYearlyCost = springCost+summerCost+winterCost+fallCost;
+        System.out.println("The yearly Maintenance Cost for Summer = " +summerCost);
+        System.out.println("The yearly Maintenance Cost for Winter = " +winterCost);
+        System.out.println("The yearly Maintenance Cost for Spring = " +springCost);
+        System.out.println("The yearly Maintenance Cost for Fall = " +fallCost);
+        System.out.println("The Total yearly Maintenance Cost = " +TotalYearlyCost);
     }
 }
